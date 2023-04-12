dfdf606c3fdf45c27d72dfb2a5b3fe7fdb9033d1
jayrugman <jasonrhardman@gmail.com>
2023-04-11 23:06:15 -0600

    script overhaul - now handles stopping for multiple monitors; using GNU getopt for options; organized code with main and more consistant syntax - added xwinwrap - updated python3 script according to bash script and settings changes - added commented line for new commant enabling playlist of videos

--- video-wallpaper.sh
+++ video-wallpaper.sh
@@ -1,153 +1,234 @@
 #!/usr/bin/env bash
 # author: Tolga MALKOC | ghostlexly@gmail.com
 # contributor: SwallowYourDreams | https://github.com/SwallowYourDreams
+# Modified by Jason Hardman https://github.com/JayRugMan
 
-# Global variables
-name="video-wallpaper"
-scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-confdir="/home/$USER/.config/video-wallpaper"
-conf="$confdir/settings.conf"
-if [ ! -d "$confdir" ] ; then
-	mkdir "$confdir"
-	touch "$conf"	
-fi
 
-# Read config
-read_config() {
-	if [ -f "$conf" ] ; then
-		source "$conf" &> /dev/null # To do: Find a more elegant way of reading variables from the config file. The shell will throw an error because it stumbles over the [video-wallpaper settings] section. For now, this is tolerable; the error message will be sent to /dev/null.
+function load_config() {
+	# Source config file with MY_PIDS and LASTFILE
+	# or creates it and then sources it
+	if [ -f "${CONF}" ] ; then
+		source <( awk 'NR!=1' ${CONF} )
 	else
-		echo 'pid=""' > "$conf"
-		echo 'lastfile=""' >> "$conf"
+	    echo "[${NAME} settings]" > "${CONF}"
+		echo 'MY_PIDS=()' >> "${CONF}"
+		echo 'LASTFILE=""' >> "${CONF}"
+		source <( awk 'NR!=1' ${CONF} )
 	fi
 }
-read_config
 
-# Write to config file
-# Parameters: $1: pid | $2: Video file
-update_config() {
-	echo "[$name settings]" > "$conf"
-	if [ ${#1} -gt 0 ] ; then
-		pid=$1
-	else
-		pid=""
+
+function update_config() {
+	# Write to config file
+	# Parameters: $1: Video file
+
+	local new_file="${1}"; shift
+	echo "[${NAME} settings]" > "${CONF}"
+
+	if [[ ! -z ${new_file} ]] ; then
+		LASTFILE="${new_file}"
 	fi
-	if [ ${#2} -gt 0 ] ; then
-		lastfile="$2"
-	fi
-	echo "pid=$pid" >> "$conf"
-	echo "lastfile=$lastfile" >> "$conf"
+
+	echo "MY_PIDS=( ${MY_PIDS[@]} )" >> "${CONF}"
+	echo "LASTFILE=${LASTFILE}" >> "${CONF}"
 }
 
-# Start video wallpaper playback
-start() {
+
+function start() {
+	# Start video wallpaper playback
 	# If there is an active video wallpaper, stop it first.
-	if [ ${#pid} -gt 0 ] ; then
+	if [ ${#MY_PIDS} -gt 0 ] ; then
 		stop
 	fi
-	VIDEO_PATH="$1"
-	SCREENS=`xrandr | grep " connected\|\*" | pcregrep -o1 '([0-9]{1,}[x]{1,1}[0-9+]{1,}) \('`
-	for item in $SCREENS
-	do
-		"$scriptdir"/xwinwrap -g $item -fdt -ni -b -nf -un -o 1.0 -- mpv -wid WID --loop --no-audio "$VIDEO_PATH" & disown
+	video_path="${1}"
+	screens=`xrandr | grep " connected\|\*" | pcregrep -o1 '([0-9]{1,}[x]{1,1}[0-9+]{1,}) \('`
+	for item in ${screens}; do
+		"${SCRIPTDIR}"/xwinwrap -g ${item} -fdt -ni -b -nf -un -o 1.0 -- mpv -wid WID --loop --no-audio "${video_path}" & disown
+		##JH "${SCRIPTDIR}"/xwinwrap -g ${item} -fdt -ni -b -nf -un -o 1.0 -- mpv -wid WID --loop --no-audio --playlist="/home/jason/.config/video-wallpaper/playlist.txt" & disown
+		MY_PIDS=( ${MY_PIDS[@]} $! )
 	done
-	update_config $! "\"$VIDEO_PATH\""
+	update_config "\"$video_path\""
 }
 
-stop() {
-	if [ ${#pid} -gt 0 ] ; then
-		echo "Stopping $name."
-		kill "$pid"
+
+function stop() {
+	# Uses PIDS in conf file to kill running xwinwrap instances
+	if [ ${#MY_PIDS} -gt 0 ] ; then
+		echo "Stopping ${NAME}."
+		for pid in ${MY_PIDS[@]}; do
+			kill ${pid}
+		done
 	else
 		echo "No active video wallpaper found."
 	fi
-	update_config "" "\"$lastfile\""
+	MY_PIDS=(); update_config "\"${LASTFILE}\""
 }
 
-# Start / disable playback of video file on system startup.
-# Parameters: $2 = true|false $3 = videofile
-startup() {
-	startup=""
-	if [ "$2" == "true" ] ; then
+
+function startup() {
+	# Start / disable playback of video file on system startup.
+	# Parameters: $1 = true|false $2 = videofile
+
+	local is_start="${1}"; shift
+	local videofile=""
+	local startup_file="/home/${USER}/.config/autostart/${NAME}.desktop"
+
+	if [[ ${is_start} == "true" ]]; then  # if true was designated by user
 		echo "Enabling startup of video wallpaper."
-		startup="true"
-		videofile="$3"
-	elif [ "$2" == "false" ] ; then
+		videofile="${1}"
+
+	elif [[ ${is_start} == "false" ]]; then  # if false was designated by user
 		echo "Disabling startup of video wallpaper."
-		startup="false"
-		videofile="$lastfile"
-	else
-		echo "Illegal startup parameter."
+		videofile="${LASTFILE}"
+
+	else  # if anything else was designated by user
+		usage "Illegal startup parameter"
 		exit 1
 	fi
-	LAUNCH_SCRIPT="bash -c '\"$scriptdir/$name.sh\" --start \"$videofile\"'"
-	printf "[Desktop Entry]\nType=Application\nExec=$LAUNCH_SCRIPT\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=$startup\nName=$name" > "/home/$USER/.config/autostart/$name.desktop"
+
+	local launch_script="bash -c '\"${SCRIPTDIR}/${NAME}.sh\" --start --video-file \"${videofile}\"'"
+	##JH printf "[Desktop Entry]\nType=Application\nExec=${launch_script}\nHidden=false\nNoDisplay=false\nX-GNOME-Autostart-enabled=${is_startup}\nName=${NAME}" > "/home/${USER}/.config/autostart/${NAME}.desktop"
+	cat <<EOF > "${startup_file}"
+[Desktop Entry]
+Type=Application
+Exec=${launch_script}
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=${is_startup}
+Name=${NAME}
+EOF
 }
 
-# Checks if a (video) file exists. Displays and error and stops the script if it doesn't
-# $1 = the file to be checked
-file_exists() {
-	if [ ! -f "$1" ] ; then
-		echo "Error. File does not exist: $1"
+
+function file_exists() {
+	# Checks if a (video) file exists. Displays and error and stops the script if it doesn't
+	# $1 = the file to be checked
+	if [ ! -f "${1}" ]; then
+		usage "File named \"${1}\" does not exist"
 		exit 1
 	fi
 }
 
 # get arguments
-print_help() {
-    echo "Usage: ./$name.sh [--start] [--stop] [--startup true|false] \"video_path.mp4\""
-    echo ""
-    echo "--start Start playback of video file."
-    echo ""
-    echo "--stop Stop active playback."
-    echo ""
-    echo "--startup Start/disable playback of video file on system startup."
-    echo ""
+function usage() {  #print_help
+	# prints usage with optional warning string
+	if [[ ! -z "${@}" ]] ; then
+		echo "ERROR: ${@}" >&2
+	fi
+
+    cat <<EOF
+
+Usage: ./${NAME}.sh [--start|--stop|--startup [true|false] ] --video-file "video_path.mp4"
+
+--start                Start playback of video file.
+
+--stop                 Stop active playback.
+
+--startup              Start/disable playback of video file on system startup.
+
+-f | --video-file      The video file
+
+*                      Gets this helpful output
+
+EOF
 }
 
-# If missing parameters, show help
-if [ $# = 0 ]
-    then print_help
-        exit 1
-fi
 
-# Parse parameters
-while true
-    do if [ $# -gt 0 ]
-        then case $1 in
-            --startup*)
-				file_exists "$3"
-                startup "${@}"
-                exit 0
-            ;;
-            
-			--start*)
-				if [ ${#2} -gt 0 ]; then
-					file_exists "$2"
-					start "$2"
+function and_action() {
+	# Parse parameters with gnu getopt (not getopts)
+
+	TEMP=$(getopt -o f: --long start,stop,startup:,video-file: -n "${NAME}" -- "$@")
+
+	if [ $? != 0 ] ; then usage "BAD Option" ; exit 1 ; fi
+
+	eval set -- "$TEMP"  # sets up getopt
+
+	local vid_file=""
+	local to_execute=""
+
+	while true; do
+		case "${1}" in
+
+			-f | --video-file ) 
+				vid_file="${2}"; shift 2
+				file_exists "${vid_file}"
+				;;
+
+			--start )
+				if [[ -z "${to_execute}" ]]; then
+					to_execute="start"
+					shift
 				else
-					print_help
+					usage "Conflicting options chosen"
 					exit 1
 				fi
-				exit 0
-			;;
-			
-            --stop*)
-				stop
-				exit 2
-            ;;
+				;;
 
-            --*)
-                print_help
-                exit 1
-            ;;
+			--stop )
+				if [[ -z "${to_execute}" ]]; then
+					to_execute="stop"
+					shift
+				else
+					usage "Conflicting options chosen"
+					exit 1
+				fi
+				;;
 
-            *)
-                break
-            ;;
-        esac
-    else
-        echo "No video path given!"
-        exit 2
-    fi
-done
+			--startup )
+				if [[ -z "${to_execute}" ]]; then
+					local is_startup="${2}"; shift 2
+					to_execute="startup ${is_startup}"
+				else
+					usage "Conflicting options chosen"
+					exit 1
+				fi
+				;;
+
+			-- ) shift; break ;;  # this should handle if a paramter needing an arg has no arg
+			* ) break;;
+		esac
+	done
+
+	if [[ "${to_execute}" == "stop" ]] || ! ${is_startup} 2>/dev/null; then
+		eval "${to_execute}"
+	elif [[ ! -z "${to_execute}" ]]; then  # the only other options are start and startup
+		if [[ ! -z ${vid_file} ]]; then eval "${to_execute} ${vid_file}"
+		else usage "A file is required for this option"; exit 1
+		fi
+	else
+		usage "No execution option chosen"
+		exit 1
+	fi
+}
+
+
+function main() {
+	# The Main Event
+
+	# GLOBALS
+	NAME="video-wallpaper"
+	SCRIPTDIR="$(dirname $(realpath "${0}"))"
+	CONFDIR="/home/${USER}/.config/video-wallpaper"
+	CONF="${CONFDIR}/settings.conf"  # contains PIDS and lastfile parameters
+	PIDS=()
+	LASTFILE=""
+	if [ ! -d "$CONFDIR" ] ; then
+		mkdir "$CONFDIR"
+		touch "$CONF"	
+	fi
+
+	# Load Config file
+	load_config  # Defines saved values of PIDS and LASTFILE
+
+	# Execute designated action(s)
+	and_action ${@}
+}
+
+
+# If missing parameters, show usage, otherwise, run program
+if [[ -z ${@} ]]; then
+	usage "No parameters provided"
+    exit 1
+else
+	main ${@}
+fi
